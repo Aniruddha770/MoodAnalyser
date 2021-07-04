2cc2b9e6aec79322df4932a8edb70fd6cc9846ff
Aniruddha770 <patilanirudha38@gmail.com>
2021-07-04 18:11:05 +0530

    Test case added

---
+++ src/test/java/TU_MoodAnalyser.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TU_MoodAnalyser {
+    //UC1-TC1.1
+    @Test
+    void givenIAmInSadMood_shouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
+        Assertions.assertEquals(mood, "SAD");
+    }
+
+    //UC1-TC1.2
+    @Test
+    void givenIAmInAnyMood_shouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String mood = moodAnalyser.analyseMood();
+        Assertions.assertEquals(mood, "HAPPY");
+    }
+
+    //UC1-TC-1.1 Repeat
+    @Test
+    void givenIAmInSadMood_shouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
+        Assertions.assertEquals(mood, "SAD");
+    }
+    //UC1-TC-1.2 Repeat
+    @Test
+    void givenIAmInHappyMood_shouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+        String mood = moodAnalyser.analyseMood();
+        Assertions.assertEquals(mood, "HAPPY");
+    }
+}
